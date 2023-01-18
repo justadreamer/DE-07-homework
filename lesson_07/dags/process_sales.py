c11c787963a0b815fc184115fd92d24bf1fe0738
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-01-18 23:42:33 +0100

    complete process_sales.py airflow pipeline

--- lesson_07/dags/process_sales.py
+++ lesson_07/dags/process_sales.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
+import json
 
 DEFAULT_ARGS = {
     'depends_on_past': False,
@@ -12,22 +13,35 @@ DEFAULT_ARGS = {
     'retry_delay': 30,
 }
 
+DATE = "{{ execution_date | ds }}"
+BASE_DIR = "/tmp/sales"
+RAW_DIR = f'{BASE_DIR}/raw/{DATE}'
+STG_DIR = f'{BASE_DIR}/stg/{DATE}'
+HEADERS = {'Content-Type':'application/json'}
 
 dag = DAG(
     dag_id='process_sales',
-    start_date=datetime(2023, 1, 9),
-    end_date=datetime(2023, 1, 11),
+    start_date=datetime(2022, 8, 9),
+    end_date=datetime(2022, 8, 12),
     schedule_interval="0 1 * * *",
     catchup=True,
     default_args=DEFAULT_ARGS,
 )
 
 extract_data_from_api = SimpleHttpOperator(
+    http_conn_id="job1",
+    endpoint="/",
+    data=json.dumps({'date': DATE, 'raw_dir': RAW_DIR}),
+    headers=HEADERS,
     task_id='extract_data_from_api',
     dag=dag,
 )
 
 convert_to_avro = SimpleHttpOperator(
+    http_conn_id="job2",
+    endpoint="/",
+    data=json.dumps({'raw_dir': RAW_DIR, 'stg_dir': STG_DIR}),
+    headers=HEADERS,
     task_id='convert_to_avro',
     dag=dag,
 )
