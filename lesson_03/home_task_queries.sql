441388d70452d1ebb15568ba7d2ab52fc0e87559
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-02-14 11:26:21 +0100

    lesson_03 fix queries grouping by actor_id

--- lesson_03/home_task_queries.sql
+++ lesson_03/home_task_queries.sql
@@ -22,14 +22,16 @@ ORDER BY films DESC;
 Результат відсортувати за спаданням.
 */
 -- SQL code goes here...
+-- Must group by actor_id, because there are actors with a different id, but with the same names (first and last)
 SELECT actor
-FROM (SELECT CONCAT(a.first_name, ' ', a.last_name) actor,
+FROM (SELECT a.actor_id,
+             CONCAT(a.first_name, ' ', a.last_name) actor,
              COUNT(i.inventory_id)                  rentals
       FROM actor a
                INNER JOIN film_actor fa ON fa.actor_id = a.actor_id
                INNER JOIN inventory i ON i.film_id = fa.film_id
                INNER JOIN rental r ON r.inventory_id = i.inventory_id
-      GROUP BY actor
+      GROUP BY a.actor_id, actor
       ORDER BY rentals DESC LIMIT 10) temp;
 
 
@@ -62,20 +64,25 @@ FROM (SELECT c.name        category,
 SELECT f.title
 FROM film f
          LEFT JOIN inventory i ON i.film_id = f.film_id
-WHERE i.inventory_id IS NULL;
+WHERE i.inventory_id IS NULL
+ORDER BY title;
 
 /*
 5.
 Вивести топ 3 актори, які найбільше зʼявлялись в категорії фільмів “Children”.
 */
 -- SQL code goes here...
+
+-- Must group by actor_id, because there are actors with a different id, but with the same name,
+-- so different actors in fact
 SELECT actor
-FROM (SELECT CONCAT(a.first_name, ' ', a.last_name) actor,
+FROM (SELECT a.actor_id,
+             CONCAT(a.first_name, ' ', a.last_name) actor,
              COUNT(fa.film_id)                      films
       FROM actor a
                INNER JOIN film_actor fa ON fa.actor_id = a.actor_id
                INNER JOIN film_category fc ON fa.film_id = fc.film_id
                INNER JOIN category c ON c.category_id = fc.category_id
       WHERE c.name = 'Children'
-      GROUP BY actor
-      ORDER BY films DESC LIMIT 3) temp;
+      GROUP BY a.actor_id, actor
+      ORDER BY films DESC, actor LIMIT 3) temp;
