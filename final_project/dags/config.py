f2fafcfba5e85fbdbf8389fc091cc5fce252e5db
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-02-26 18:46:29 +0100

    comments explaining config file motivation

--- final_project/dags/config.py
+++ final_project/dags/config.py
@@ -1,3 +1,7 @@
+# common constants used in all DAGS
+# normally would be set thru the environment -placed in the repo just because this is a learning project
+# also no secrets are exposed - authentication is done using `gcloud auth login` on the local machine
+
 RAW_BUCKET = 'de07-final-raw'
 PROJECT_ID = 'de-07-376021'
 DEFAULT_ARGS = {
