49fe6be4ea0b10f65a5fc18125a0190a0974b177
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-02-26 18:33:03 +0100

    moved constants to config file

---
+++ final_project/dags/config.py
@@ -0,0 +1,10 @@
+RAW_BUCKET = 'de07-final-raw'
+PROJECT_ID = 'de-07-376021'
+DEFAULT_ARGS = {
+    'depends_on_past': False,
+    'email': ['admin@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': 30,
+}
--- final_project/dags/process_customers.py
+++ final_project/dags/process_customers.py
@@ -4,18 +4,7 @@ from airflow.providers.google.cloud.operators.bigquery import \
     BigQueryCreateEmptyTableOperator, \
     BigQueryInsertJobOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-
-# These params normally would be provided via env or config file, however for study purposes they are here
-RAW_BUCKET = 'de07-final-raw'
-PROJECT_ID = 'de-07-376021'
-DEFAULT_ARGS = {
-    'depends_on_past': False,
-    'email': ['admin@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 2,
-    'retry_delay': 30,
-}
+from config import *
 
 
 dag = DAG(
